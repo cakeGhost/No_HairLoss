0123c665001a13bf5d2a8914fce69ea2a9aa1ddb
cakeGhost <94977962+cakeGhost@users.noreply.github.com>
2021-11-26 15:14:00 +0900

    Update HomeCollectionViewCell.swift
    is Selected 추가

--- No_HairLoss/Controllers/01_Home/HomeCollectionViewCell.swift
+++ No_HairLoss/Controllers/01_Home/HomeCollectionViewCell.swift
@@ -5,5 +5,21 @@
 //  Created by betty on 2021/11/24.
 //
 
-import Foundation
 
+import Foundation
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+            }
+        }
+    }
+    
+}
